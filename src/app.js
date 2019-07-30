0a2067eec5818189854be775d87e0c8710bf1cf5
Felipe Travassos Rodrigues <ftrodrigues@banpara.com>
2019-07-30 14:24:22 -0300

    Adicionando o a listagem usando o get e o find

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ const router = express.Router();
 // Connect on Mongo
 mongoose.connect('mongodb://localhost:27017/ndstr', {useNewUrlParser: true});
 
+// Carrega os Models
+const Product = require('./models/product');
+
 // Carrega as rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route')
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,8 +1,33 @@
 'use strict'
 
-exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product
+        .find({ active: true }, 'title price slug')
+        .then(data => {
+            res.status(201).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
 }
+
+exports.post = (req, res, next) => {
+    var product = new Product(req.body);
+    Product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Falha ao cadastrar o produto!',
+                data: e
+            });
+        });
+};
 exports.put = (req, res, next) => {
     const id = req.params.id;
     res.status(201).send({
