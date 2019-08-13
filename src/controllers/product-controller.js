bb5a8763270cee9e4d42fb63ed0b59ab69819456
Felipe Travassos Rodrigues <ftrodrigues@banpara.com>
2019-08-13 11:47:05 -0300

    Implementando o fluent validators

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 // Metodo para GET básico - SELECT
 exports.get = (req, res, next) => {
@@ -27,7 +28,7 @@ exports.getBySlug = (req, res, next) => {
             res.status(400).send(e);
         });
 }
- // Metodo para buscar  por TAG
+// Metodo para buscar  por TAG
 exports.getByTag = (req, res, next) => {
     Product
         .find({
@@ -54,6 +55,16 @@ exports.getById = (req, res, next) => {
 
 // Metodo para adicionar um registro - INSERT
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O titulo deve conter pelo menos 3 caracteres! :(');
+    contract.hasMinLen(req.body.slug, 3, 'O Slug deve conter pelo menos 3 caracteres! :(');
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres! :(');
+
+    // Se os dados forem válidos:
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
     var product = new Product(req.body);
     product
         .save()
